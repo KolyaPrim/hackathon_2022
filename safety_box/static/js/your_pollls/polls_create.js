676ba44a603ae1cfb26f5cfc6ead6692bcda9914
Ollie <feoktistov_oi@farpost.com>
2022-11-26 17:39:17 +1000

    some changes in poll creating page

---
+++ safety_box/static/js/your_pollls/polls_create.js
@@ -0,0 +1,15 @@
+window.add_question = function () {
+    console.log('hello again')
+}
+
+window.add_radio = function () {
+
+}
+
+window.add_checkbox = function () {
+
+}
+
+window.add_text_field = function () {
+    
+}
