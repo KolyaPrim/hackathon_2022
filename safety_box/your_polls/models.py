4915fb96bdf8bf6c9dfcd2a8b409b7f54e3a23e0
gudoshnikov_na <gudoshnikov_na@farpost.com>
2022-11-27 01:34:12 +1000

    Added save poll object

--- safety_box/your_polls/models.py
+++ safety_box/your_polls/models.py
@@ -7,26 +7,25 @@ User = get_user_model()
 
 class Poll(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    css_file = models.FileField(upload_to='polls_styles/')
+    css_file = models.FileField(upload_to='polls_styles/', default='')
     title = models.CharField(max_length=200)
-    description = models.TextField()
+    description = models.TextField(blank=True)
 
 
 class Question(models.Model):
     poll = models.ForeignKey(Poll, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
-    description = models.TextField()
+    description = models.TextField(blank=True)
 
 
 class Variant(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     label = models.CharField(max_length=100)
     type = models.CharField(max_length=50)
-    value = models.TextField()
-    name = models.CharField(max_length=50)
+    value = models.TextField(blank=True)
+    name = models.CharField(max_length=50, blank=True)
 
 
 class Answer(models.Model):
     variant = models.ForeignKey(Variant, on_delete=models.CASCADE)
     value = models.TextField()
-
