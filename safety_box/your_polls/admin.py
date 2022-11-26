56f987a9166ed541e833ba78d03147f40103e6d5
Ollie <feoktistov_oi@farpost.com>
2022-11-26 12:49:01 +1000

    Added required models and registered them in admin.py

--- safety_box/your_polls/admin.py
+++ safety_box/your_polls/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Poll, Question, Variant, Answer
 
-# Register your models here.
+
+admin.site.register(Poll)
+admin.site.register(Question)
+admin.site.register(Variant)
+admin.site.register(Answer)
