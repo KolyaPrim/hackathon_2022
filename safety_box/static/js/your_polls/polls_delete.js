be1fc943128adfcbc54bf25569ebaf479604756f
gudoshnikov_na <gudoshnikov_na@farpost.com>
2022-12-02 15:12:55 +1000

    Fix js on main page

--- safety_box/static/js/your_polls/polls_delete.js
+++ safety_box/static/js/your_polls/polls_delete.js
@@ -1,4 +1,4 @@
-function delete_poll(id) {
+function delete_poll(id,tag_id) {
     $.ajax({
         url: '/poll/delete/' + id + '/',
         headers: {
@@ -8,6 +8,10 @@ function delete_poll(id) {
         dataType: 'json',
         success: function (data) {
             document.getElementById('poll_' + id).remove()
+            let length_block = $(`#block_tag_${tag_id} div[id^='poll_']`).length
+            if(length_block === 0){
+                 $(`#block_tag_${tag_id}`).remove()
+            }
         },
         error: function (xhr, errmsg, err) {
             $('#modal_message_label').text('Error')
