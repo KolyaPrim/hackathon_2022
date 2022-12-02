aecc894819928a27f48ce257e2acd3399c2440e1
gudoshnikov_na <gudoshnikov_na@farpost.com>
2022-12-02 15:13:40 +1000

    Fix analytics if answers no found

--- safety_box/analytics/plotter.py
+++ safety_box/analytics/plotter.py
@@ -13,22 +13,59 @@ class AnalyticsPlotter:
     def __init__(self, poll: Poll):
         self.poll = poll
 
+    @staticmethod
+    def check_answers(poll) -> bool:
+        variants_id_list = []
+        questions = Question.objects.filter(poll=poll)
+
+        for question in questions:
+            variants = Variant.objects.filter(question=question)
+            [variants_id_list.append(item.id) for item in variants]
+
+        list_answers_id = list(Answer.objects.all().values_list('variant', flat=True))
+        for i in variants_id_list:
+            if i in list_answers_id:
+                return True
+        return False
+
+    @staticmethod
+    def generate_question_bar(variants_data):
+        fig = px.bar(
+            pd.DataFrame(
+                variants_data
+            ),
+            x='id',
+            y='count',
+            text="label"
+        )
+        fig.update_xaxes(visible=False, showticklabels=False)
+        return fig
+
     def build_analytics_app(self) -> str:
+
+        if not self.check_answers(self.poll):
+            name = str(time.time()).replace('.', '')
+            app = DjangoDash(name=name)
+            app.layout = dash.html.Div([
+                dash.html.H1('Answers no found'),
+            ])
+
+            return name
         poll_views = PollsGet.objects.filter(poll=self.poll)
         poll_views_pd = pd.DataFrame([
-            {"date":poll_view.datetime}
+            {"date": poll_view.datetime}
             for poll_view
             in poll_views])
         poll_views_pd['date'] = poll_views_pd['date'].round("d")
         poll_views_by_dates = poll_views_pd.groupby('date')['date'].count()
-        poll_views_by_dates = pd.DataFrame({'date': poll_views_by_dates.index,'count': poll_views_by_dates})
+        poll_views_by_dates = pd.DataFrame({'date': poll_views_by_dates.index, 'count': poll_views_by_dates})
 
         questions = Question.objects.filter(poll=self.poll)
         questions_data = {
             question.title: [
                 {
                     'id': variant.id,
-                    'label':variant.label,
+                    'label': variant.label,
                     'count': len(Answer.objects.filter(variant=variant))
                 }
                 for variant
@@ -49,7 +86,7 @@ class AnalyticsPlotter:
             in questions_data.items()
         ]
 
-        name = str(time.time()).replace('.','')
+        name = str(time.time()).replace('.', '')
         app = DjangoDash(name=name)
         app.layout = dash.html.Div([
             dash.html.H1('Slice of visits by dates'),
@@ -60,15 +97,3 @@ class AnalyticsPlotter:
         ])
 
         return name
-
-    def generate_question_bar(self, variants_data):
-        fig = px.bar(
-                        pd.DataFrame(
-                            variants_data
-                        ),
-                        x='id',
-                        y='count',
-                        text="label"
-                    )
-        fig.update_xaxes(visible=False, showticklabels=False)
-        return fig
